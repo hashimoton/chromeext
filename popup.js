2c81b9657bb03c43f130397eb8609f5ed4c9e777
hashimoton <hashimoton@users.noreply.github.com>
2023-02-18 11:06:50 +0900

    Load libraries dynamically in popup.js

--- popup.js
+++ popup.js
@@ -1,49 +1,28 @@
 // popup.js
 
-alert('popup.js'); // Cannot use console.log() here.
-let Popup = {};
+alert('popup.js!'); // Cannot use console.log() here.
 
 import {log, load_options} from "./common.js";
 load_options();
-
-
-async function load_lib(lib_name) {
-  log('Loading ' + lib_name);
-  Popup[lib_name] = await import('./lib/' + lib_name + '.js');
-};
-
-
-function prepare(lib, action, options) {
-  let request = {
-    lib: lib,
-    action: action,
-  };
-  options = document.getElementById('options').innerText;
-  request.payload = Popup[lib][action].prepare(options);
-  
-  return request;
-}
-
-
-function callback(response) {
-  log(response);
-  Popup[response.request.lib][response.request.action].callback(response);
-}
-
 
 // Activate actions
 const action_buttons = document.querySelectorAll('.action');
 for(let i = 0; i < action_buttons.length; i++) {
   let action_button = action_buttons[i];
-  const lib = action_button.closest('.lib').dataset.lib;
-  load_lib(lib);
+  const lib_name = action_button.closest('.lib').dataset.lib;
   const action = action_button.dataset.action;
 
   action_button.addEventListener('click', async function() {
-    const request = prepare(lib, action, options);
+    const options = document.getElementById('options').innerText;
+    const lib = await import('./lib/' + lib_name + '.js');
+    const request = {
+      lib: lib_name,
+      action: action,
+      payload: lib[action].prepare(options)
+    };
     
-    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, request, callback);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, request, lib[action].callback);
     });
   });
 }
